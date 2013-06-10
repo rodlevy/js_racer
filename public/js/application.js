3dec962596768ade6a8d341066e4df25d40c46cc
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-10 16:30:59 -0500

    front end mvp

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,39 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+$(document).ready(function() {
+
+  $('.winner').hide();
+
+
+  $(document).on('keyup', function(event) {
+
+    if (event.which === 81) {
+      var currentCol = $('#player1_strip .active');
+
+      currentCol.removeClass('active');
+      currentCol.next().addClass('active');
+      if (currentCol.next().attr('id') === "finish_line1") {
+        $(document).unbind('keyup');
+        $('.winner h2').text('Player one rocks!!!');
+        $('.winner').show();
+      }
+    }
+    else if (event.which === 80) {
+      var currentCol = $('#player2_strip .active');
+
+      currentCol.removeClass('active');
+      currentCol.next().addClass('active');
+
+      if (currentCol.next().attr('id') === "finish_line2") {
+        $(document).unbind('keyup');
+        $('.winner h2').text('Player two rocks!!!');
+        $('.winner').show();
+      }
+    }
+  });
 });
+
+
+
+// q = 81
+// p = 80
